587949e0240493b2eee5ebb87ee8aea47e7570ef
ErwannGui <erwann.guillevic@ynov.com>
2018-02-27 17:36:36 +0100

    controller et views
    vous avez tout a disposition vous avez juste a travailler sur le html
    css

---
+++ projet_emnia/src/Lemnia/CustomerPortalBundle/Controller/SubscriptionController.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace Lemnia\CustomerPortalBundle\Controller;
+
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Method;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Security;
+use Symfony\Component\Security\Core\Exception\AccessDeniedException;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Lemnia\UserBundle\Entity\User;
+
+class SubscriptionController extends Controller
+{
+
+	/**
+     * @Route("/subscription", name="customer_portal_bundle_subscription")
+     * On peut définir des droits spécifiques à cette route afin d'en limiter l'accès
+     */
+    public function listSubscriptionAction(Request $request){
+
+        /*$em = $this->getDoctrine()->getManager();
+        $users = $em->getRepository('LemniaUserBundle:User')->findAll();*/
+
+        return $this->render('subscription/subscription.html.twig', [
+            'base_dir' => realpath($this->getParameter('kernel.project_dir')).DIRECTORY_SEPARATOR,
+        ]);
+    }
+}
---
+++ projet_emnia/src/Lemnia/CustomerPortalBundle/Controller/UserController.php
@@ -0,0 +1,43 @@
+<?php
+
+namespace Lemnia\CustomerPortalBundle\Controller;
+
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Method;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Security;
+use Symfony\Component\Security\Core\Exception\AccessDeniedException;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Lemnia\UserBundle\Entity\User;
+
+class UserController extends Controller
+{
+
+    /**
+     * @Route("/users", name="customer_portal_bundle_list_user")
+     * On peut définir des droits spécifiques à cette route afin d'en limiter l'accès
+     */
+    public function listUserAction(Request $request){
+
+        $em = $this->getDoctrine()->getManager();
+        $users = $em->getRepository('LemniaUserBundle:User')->findAll();
+
+        return $this->render('users/users.html.twig', [
+            'base_dir' => realpath($this->getParameter('kernel.project_dir')).DIRECTORY_SEPARATOR, 'users' => $users,
+        ]);
+    }
+
+    /**
+     * @Route("/user", name="customer_portal_bundle_home_page")
+     * On peut définir des droits spécifiques à cette route afin d'en limiter l'accès
+     */
+    public function userAction(Request $request){
+
+		$user = $this->getUser();
+
+        return $this->render('users/user.html.twig', [
+            'base_dir' => realpath($this->getParameter('kernel.project_dir')).DIRECTORY_SEPARATOR, 'user' => $user,
+        ]);
+    }
+}
