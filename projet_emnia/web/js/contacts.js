4bb460303ebec71e0b0adff571d7675fa9add711
CorentinDupont <dupont.corentin44@gmail.com>
2018-03-05 00:13:58 +0100

    Page contacts - afficher et ajouter

---
+++ projet_emnia/web/js/contacts.js
@@ -0,0 +1,10 @@
+
+//Add contact window opening
+$('#co_fabAddContact_contacts').click(function(){
+    $('#co_addContactFormBlock_contacts, .co_blueWindowArrowBottomRight').fadeIn();
+});
+
+$('#co_addContactWindowClose_contacts').click(function(){
+    $('#co_addContactFormBlock_contacts, .co_blueWindowArrowBottomRight').fadeOut();
+});
+
