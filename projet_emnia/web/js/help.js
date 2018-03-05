c06041092c63d8c05d1912bb68688d3b40d32cb6
CorentinDupont <dupont.corentin44@gmail.com>
2018-03-05 16:12:56 +0100

    Début fab blue window envoyer ticket

---
+++ projet_emnia/web/js/help.js
@@ -0,0 +1,10 @@
+
+//send message window opening
+$('#co_sendMessageFab_help').click(function(){
+    $('#co_sendMessageFormBlock_help, .co_blueWindowArrowBottomRight').fadeIn();
+});
+
+$('#co_sendMessageWindowClose_help').click(function(){
+    $('#co_sendMessageFormBlock_help, .co_blueWindowArrowBottomRight').fadeOut();
+});
+
