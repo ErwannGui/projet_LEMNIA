9a89f499e84590c930a5ecf6f35b22e40642e2eb
CorentinDupont <dupont.corentin44@gmail.com>
2018-03-07 13:05:36 +0100

    Discussion modal

--- projet_emnia/web/js/help.js
+++ projet_emnia/web/js/help.js
@@ -8,3 +8,20 @@ $('#co_sendMessageWindowClose_help').click(function(){
     $('#co_sendMessageFormBlock_help, .co_blueWindowArrowBottomRight').fadeOut();
 });
 
+var discussionModal = $('#co_discussionModalContainer_help');
+//modal discussion
+$('#co_openDiscussionModalButton_help').click(function() {
+    discussionModal.css('display', 'flex');
+    discussionModal.animate({
+        opacity: 1
+    });
+});
+
+$('#co_modalDiscussionCloseButton').click(function(){
+    discussionModal.animate({
+        opacity:0
+    }, function(){
+        $('#co_discussionModalContainer_help').css('display', 'none');
+    });
+});
+
