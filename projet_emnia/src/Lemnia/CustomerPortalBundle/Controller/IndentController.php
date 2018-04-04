09b5866479a1ee61bf920b1ba4695caeba8ecad2
ErwannGui <erwann.guillevic@ynov.com>
2018-04-04 15:54:47 +0200

    api select commandes

--- projet_emnia/src/Lemnia/CustomerPortalBundle/Controller/IndentController.php
+++ projet_emnia/src/Lemnia/CustomerPortalBundle/Controller/IndentController.php
@@ -20,13 +20,71 @@ class IndentController extends Controller
      */
     public function listIndentAction(Request $request){
 
-        /*$em = $this->getDoctrine()->getManager();
-        $users = $em->getRepository('LemniaUserBundle:User')->findAll();*/
+        function CallAPI($method, $apikey, $url, $data = false){
+
+            $curl = curl_init();
+            $httpheader = ['DOLAPIKEY: '.$apikey];
+         
+            switch ($method)
+            {
+                case "POST":
+                    curl_setopt($curl, CURLOPT_POST, 1);
+                    $httpheader[] = "Content-Type:application/json";
+         
+                    if ($data)
+                        curl_setopt($curl, CURLOPT_POSTFIELDS, $data);
+         
+                    break;
+                case "PUT":
+         
+                curl_setopt($curl, CURLOPT_CUSTOMREQUEST, 'PUT');
+                    $httpheader[] = "Content-Type:application/json";
+         
+                    if ($data)
+                        curl_setopt($curl, CURLOPT_POSTFIELDS, $data);
+         
+                    break;
+                default:
+                    if ($data)
+                        $url = sprintf("%s?%s", $url, http_build_query($data));
+            }
+         
+            // Optional Authentication:
+            //    curl_setopt($curl, CURLOPT_HTTPAUTH, CURLAUTH_BASIC);
+            //    curl_setopt($curl, CURLOPT_USERPWD, "username:password");
+            curl_setopt($curl, CURLOPT_SSL_VERIFYHOST, false);
+            curl_setopt($curl, CURLOPT_SSL_VERIFYPEER, false);
+         
+            curl_setopt($curl, CURLOPT_URL, $url);
+            curl_setopt($curl, CURLOPT_RETURNTRANSFER, 1);
+            curl_setopt($curl, CURLOPT_HTTPHEADER, $httpheader);
+         
+            $result = curl_exec($curl);
+         
+            curl_close($curl);
+         
+            return json_decode($result, true);
+        }
+
+        $apiKey = "mp70cv82";
+        $apiUrl = "https://doli2.lemnia.net/api/index.php/";
+
+        $listIndents = array();
+        $param = ["limit" => 1000, "sortfield" => "rowid"];
+        $listIndentsResult = CallAPI("GET", $apiKey, $apiUrl."orders", $param);
+
+        if (isset($listIndentsResult["error"]) && $listIndentsResult["error"]["code"] >= "300") {
+            $error = true;
+        } else {
+            foreach ($listIndentsResult as $indent) {
+                array_push($listIndents, $indent);
+            }
+        }
 
         $user = $this->getUser();
 
         return $this->render('indent/indent.html.twig', [
-            'base_dir' => realpath($this->getParameter('kernel.project_dir')).DIRECTORY_SEPARATOR,
+            'base_dir' => realpath($this->getParameter('kernel.project_dir')).DIRECTORY_SEPARATOR, 'error' => $error, 'listIndents' => $listIndents
         ]);
     }
 }
