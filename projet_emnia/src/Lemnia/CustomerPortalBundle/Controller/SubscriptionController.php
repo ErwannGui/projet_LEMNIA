24feb9e271eb678f941ee165c860b1edec5d6495
ragorne <fabien.drap@laposte.net>
2018-04-05 11:08:47 +0200

    api contact, homepage, abonnement

--- projet_emnia/src/Lemnia/CustomerPortalBundle/Controller/SubscriptionController.php
+++ projet_emnia/src/Lemnia/CustomerPortalBundle/Controller/SubscriptionController.php
@@ -18,15 +18,80 @@ class SubscriptionController extends Controller
      * @Route("/subscription", name="customer_portal_bundle_subscription")
      * On peut définir des droits spécifiques à cette route afin d'en limiter l'accès
      */
+
     public function listSubscriptionAction(Request $request){
 
         /*$em = $this->getDoctrine()->getManager();
         $users = $em->getRepository('LemniaUserBundle:User')->findAll();*/
 
         $user = $this->getUser();
+         function CallAPI($method, $apikey, $url, $data = false){
+            $curl = curl_init();
+            $httpheader = ['DOLAPIKEY: '.$apikey];
+         
+            switch ($method)
+            {
+                case "POST":
+                    curl_setopt($curl, CURLOPT_POST, 1);
+                    $httpheader[] = "Content-Type:application/json";
+         
+                    if ($data)
+                        curl_setopt($curl, CURLOPT_POSTFIELDS, $data);
+         
+                    break;
+                case "PUT":
+         
+                curl_setopt($curl, CURLOPT_CUSTOMREQUEST, 'PUT');
+                    $httpheader[] = "Content-Type:application/json";
+         
+                    if ($data)
+                        curl_setopt($curl, CURLOPT_POSTFIELDS, $data);
+         
+                    break;
+                default:
+                    if ($data)
+                        $url = sprintf("%s?%s", $url, http_build_query($data));
+            }
+         
+            // Optional Authentication:
+            //    curl_setopt($curl, CURLOPT_HTTPAUTH, CURLAUTH_BASIC);
+            //    curl_setopt($curl, CURLOPT_USERPWD, "username:password");
+            curl_setopt($curl, CURLOPT_SSL_VERIFYHOST, false);
+            curl_setopt($curl, CURLOPT_SSL_VERIFYPEER, false);
+         
+            curl_setopt($curl, CURLOPT_URL, $url);
+            curl_setopt($curl, CURLOPT_RETURNTRANSFER, 1);
+            curl_setopt($curl, CURLOPT_HTTPHEADER, $httpheader);
+         
+            $result = curl_exec($curl);
+         
+            curl_close($curl);
+         
+            return json_decode($result, true);
+        }
+        $activity=false;
+        $apiKey = "mp70cv82";
+        $apiUrl = "https://doli2.lemnia.net/api/index.php/";
+        $listSubs = array();
+        $param = ["limit" => 1000, "sortfield" => "rowid"];
+        $listSubsResult = CallAPI("GET", $apiKey, $apiUrl."subscriptions", $param);
+        if (isset($listSubsResult["error"]) && $listSubsResult["error"]["code"] >= "300") {
+            $error = true;
+        } else {
+            foreach ($listSubsResult as $subscriptions) {
+                array_push($listSubs, $subscriptions);
+                if ( $listSubs["date"] > date() ) {
+                    $activity = true;
+                }
+                else {
+                    $activity = false;
+                }
+            }
+        }
+       
 
         return $this->render('subscription/subscription.html.twig', [
-            'base_dir' => realpath($this->getParameter('kernel.project_dir')).DIRECTORY_SEPARATOR,
+            'base_dir' => realpath($this->getParameter('kernel.project_dir')).DIRECTORY_SEPARATOR, 'error' => $error, 'subscriptions' => $listSubs, "activity" => $activity
         ]);
     }
 }
--- projet_emnia/src/Lemnia/CustomerPortalBundle/Controller/UserController.php
+++ projet_emnia/src/Lemnia/CustomerPortalBundle/Controller/UserController.php
@@ -195,9 +195,66 @@ class UserController extends Controller
 
     public function contactsAction(Request $request){
 
-
-        return $this->render('users/contacts.html.twig', [
-            'base_dir' => realpath($this->getParameter('kernel.project_dir')).DIRECTORY_SEPARATOR,
+        function CallAPI($method, $apikey, $url, $data = false){
+            $curl = curl_init();
+            $httpheader = ['DOLAPIKEY: '.$apikey];
+         
+            switch ($method)
+            {
+                case "POST":
+                    curl_setopt($curl, CURLOPT_POST, 1);
+                    $httpheader[] = "Content-Type:application/json";
+         
+                    if ($data)
+                        curl_setopt($curl, CURLOPT_POSTFIELDS, $data);
+         
+                    break;
+                case "PUT":
+         
+                curl_setopt($curl, CURLOPT_CUSTOMREQUEST, 'PUT');
+                    $httpheader[] = "Content-Type:application/json";
+         
+                    if ($data)
+                        curl_setopt($curl, CURLOPT_POSTFIELDS, $data);
+         
+                    break;
+                default:
+                    if ($data)
+                        $url = sprintf("%s?%s", $url, http_build_query($data));
+            }
+         
+            // Optional Authentication:
+            //    curl_setopt($curl, CURLOPT_HTTPAUTH, CURLAUTH_BASIC);
+            //    curl_setopt($curl, CURLOPT_USERPWD, "username:password");
+            curl_setopt($curl, CURLOPT_SSL_VERIFYHOST, false);
+            curl_setopt($curl, CURLOPT_SSL_VERIFYPEER, false);
+         
+            curl_setopt($curl, CURLOPT_URL, $url);
+            curl_setopt($curl, CURLOPT_RETURNTRANSFER, 1);
+            curl_setopt($curl, CURLOPT_HTTPHEADER, $httpheader);
+         
+            $result = curl_exec($curl);
+         
+            curl_close($curl);
+         
+            return json_decode($result, true);
+        }
+    
+        $apiKey = "mp70cv82";
+        $apiUrl = "https://doli2.lemnia.net/api/index.php/";
+        $listContacts = array();
+        $param = ["limit" => 1000, "sortfield" => "rowid"];
+        $listContactsResult = CallAPI("GET", $apiKey, $apiUrl."contacts", $param);
+        if (isset($listContactsResult["error"]) && $listContactsResult["error"]["code"] >= "300") {
+        	$error = true;
+        } else {
+            foreach ($listContactsResult as $contatcs) {
+                array_push($listContacts, $contacts);
+            }
+        }
+       
+		 return $this->render('users/contacts.html.twig', [
+            'base_dir' => realpath($this->getParameter('kernel.project_dir')).DIRECTORY_SEPARATOR, 'listContacts' => $listContacts, 'error' => $error
         ]);
     }
 }
